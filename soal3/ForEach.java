8286139b91c89d6177aa0217535e2a1cd24e2572
Paul Andrew <2381022@unai.edu>
2024-09-22 08:46:55 +0700

    TUGAS 1

--- soal3/ForEach.java
+++ soal3/ForEach.java
@@ -2,20 +2,19 @@ package soal3;
 
 import java.util.Scanner;
 
-public class ForLoop {
+public class ForEach {
     public static void main(String[] args) {
         String kumpulanKata;
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan kata kata menggunakan cara penulisan camelCase : ");
         kumpulanKata = input.next();
         char[] kataKata = kumpulanKata.toCharArray();
-        int Total = 1;
+        int totalKata = 1;
         for (char huruf : kataKata){
             if((int)huruf >= 65 && (int)huruf <= 90){
-                Total++;
+                totalKata++;
             }
         }
-        System.out.println("Jumlah kata-kata yang ada dalam kalimat adalah : " + Total);
-
+        System.out.println("Jumlah kata-kata yang ada dalam kalimat adalah : " + totalKata);
     }
 }
